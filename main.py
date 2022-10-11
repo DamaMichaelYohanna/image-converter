0632bd4fca7a44c5c3295977fcbd0a0a929caf8f
DamaMichaelYohanna <get2dama11@gmail.com>
2022-10-11 21:58:36 +0100

    add image conversion ability

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from PIL import Image
+import tkinter as tk
 
-im = Image.open("dama.jpg")
+##window = tk.Tk()
+def load_picture():
+    image_name = input("enter image name")
+    im = Image.open(image_name)
+    return im.convert("RGB")
 
-im.show()
+def save_as_format():
+    image_format = input("enter Image format")
+    image = load_picture()
+    name = 'image.' + image_format
+    print(name)
+    image.save(name)
+
+save_as_format()  
+##tkimage = tk.PhotoImage(im)
+##tk.Label(window, image=tkimage).grid()
+##window.mainloop()
