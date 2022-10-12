1521663d391faf8ef0dcc4a2ee96eed87f83b64e
DamaMichaelYohanna <get2dama11@gmail.com>
2022-10-12 09:49:52 +0100

    import image using tk file dialog

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+import os
 from PIL import Image
 import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter import ttk
 
-##window = tk.Tk()
+window = tk.Tk()
+
+class Interface(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.parent.title("Image Converter")
+        self.parent['bg'] = 'red'
+
+        self.build_ui()
+        
+    def import_image(self):
+        """function to import image from disk"""
+        file = fd.askopenfilename(title="Open file", initialdir=os.environ["HOME"])
+        return file
+        
+    def build_ui(self):
+        """ functiion to build ui """
+        ui_frame = tk.Frame(self.parent)
+        ui_frame.grid()
+        tk.Button(ui_frame, text='hello friend',
+                  command=self.import_image).grid()
+
+
+Interface(window)
+
 def load_picture():
     image_name = input("enter image name")
     im = Image.open(image_name)
@@ -14,7 +42,7 @@ def save_as_format():
     print(name)
     image.save(name)
 
-save_as_format()  
+
 ##tkimage = tk.PhotoImage(im)
 ##tk.Label(window, image=tkimage).grid()
 ##window.mainloop()
