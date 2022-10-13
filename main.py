be63e90c7de930411293f1cc39502d6f49f07c2b
DamaMichaelYohanna <get2dama11@gmail.com>
2022-10-13 16:51:08 +0100

    change background of frame and button to light grey

--- main.py
+++ main.py
@@ -58,19 +58,19 @@ class Interface(ttk.Frame):
             image_format = input_var.get()
             self.converter_object.save_as_format(image_format)
             
-        ui_frame = tk.Frame(self.parent)
+        ui_frame = tk.Frame(self.parent, bg="#f6f6f6", bd=2)
         ui_frame.grid()
-        bg = tk.Label(ui_frame, text="hello world", font='Matura 26', bg="white")
+        bg = tk.Label(ui_frame, text="hello world", font='Matura 26', bg="#f4f4f4")
         bg.grid(ipady=40)
-        tk.Button(ui_frame, text='Select Image', font='Helvetica 15',width=23,
+        tk.Button(ui_frame, text='Select Image', font='Helvetica 15',width=23,bg="#f4f4f4",
                   command=self.import_image).grid(pady=10)
         ttk.Separator(ui_frame, orient='horizontal').grid(pady=0,padx=10, sticky='we')
         input_var = tk.StringVar()
         image_format = ttk.Combobox(ui_frame,state='readonly',textvariabl=input_var,
-                     values=['jpg', 'png', 'ico'], font='matura 15')
+                     values=['jpg', 'png', 'ico'], font='matura 15',)
         image_format.set("Select Format")
         image_format.grid(pady=10, padx=10)
-        tk.Button(ui_frame, text="Convert Image",
+        tk.Button(ui_frame, text="Convert Image",bg="#f4f4f4",
                   font="Helvetica 15", width=23,
                   command=convert_call_back).grid(pady=10)
 
