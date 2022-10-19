405dd66f2e7eb4d3dfbd42c77844f56103883355
DamaMichaelYohanna <get2dama11@gmail.com>
2022-10-19 23:47:46 +0100

    add frame for github and fb link

--- main.py
+++ main.py
@@ -42,6 +42,10 @@ class Interface(ttk.Frame):
         self.select_icon=  self.select_icon.subsample(5, 5)
         self.convert_icon = tk.PhotoImage(file='assets/convert.png')
         self.convert_icon=  self.convert_icon.subsample(5, 5)
+        self.github_icon = tk.PhotoImage(file='assets/githut.png')
+##        self.select_icon=  self.select_icon.subsample(5, 5)
+        self.facebook_icon = tk.PhotoImage(file='assets/facebook.png')
+##        self.convert_icon=  self.convert_icon.subsample(5, 5)
 
         self.converter_object = ImageConversion()
 
@@ -88,6 +92,9 @@ class Interface(ttk.Frame):
         tk.Label(credit_win,
                  text="You can check on me on the below channels",
                  font='helvetica 13', bg='white').grid()
+        link_frame = tk.Frame(credit_ui)
+        link_frame.grid()
+        
 
     def help_ui(self):
         help_win = tk.Toplevel(self.parent, bg='white')
